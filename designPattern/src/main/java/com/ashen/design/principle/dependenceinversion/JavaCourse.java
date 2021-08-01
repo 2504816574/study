851ccb7d838075e08ed23c1ad0912c2d48a611a5
2504816574 <2504816574@qq.com>
2021-08-01 10:14:02 +0800

    项目_设计模式_依赖倒置原则_修改注解

--- designPattern/src/main/java/com/ashen/design/principle/dependenceinversion/JavaCourse.java
+++ designPattern/src/main/java/com/ashen/design/principle/dependenceinversion/JavaCourse.java
@@ -1,7 +1,7 @@
 package com.ashen.design.principle.dependenceinversion;
 
 /**
- * @ClassName ICourse
+ * @ClassName
  * @Author 董升
  * @Date 2021/7/31
  * @Version V1.0
@@ -11,6 +11,6 @@ public class JavaCourse implements ICourse {
 
     @Override
     public void studyCourse() {
-        System.out.println("student在学习Java课程");
+        System.out.println("studnt在学习Java课程");
     }
 }
