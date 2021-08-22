689c4f6f0a8e7796c9bcb1cdf7386aac0e132e94
2504816574 <2504816574@qq.com>
2021-08-22 11:17:38 +0800

    设计模式-外观模式

---
+++ designPattern/src/main/java/com/ashen/design/pattern/structural/facade/QualifyService.java
@@ -0,0 +1,11 @@
+package com.ashen.design.pattern.structural.facade;
+
+/**
+ * 资格校验
+ */
+public class QualifyService {
+    public boolean isAvailable(PointsGift pointsGift) {
+        System.out.println("校验" + pointsGift.getName() + " 积分资格通过,库存通过");
+        return true;
+    }
+}
