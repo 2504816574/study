0f1626de8595ab1b56ce97f4189d0f8baea25402
2504816574 <2504816574@qq.com>
2021-10-10 00:01:41 +0800

    MyBatis项目-Druid连接池

--- MyBatis/src/main/java/com/ashen/mybatis/config/DataSourceConfig1.java
+++ MyBatis/src/main/java/com/ashen/mybatis/config/DataSourceConfig1.java
@@ -1,19 +1,20 @@
 package com.ashen.mybatis.config;
 
 
+import com.alibaba.druid.pool.DruidDataSource;
+import lombok.Data;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 import javax.sql.DataSource;
+import java.sql.SQLException;
 
 
 /**
@@ -22,40 +23,108 @@ import javax.sql.DataSource;
  * @Version V1.0
  * @Description:
  **/
+@Data
+@ConfigurationProperties(prefix = "spring.datasource.db01.druid")
 @Configuration
-@MapperScan(basePackages = "com.ashen.mybatis.mapper.db01", sqlSessionFactoryRef = "db01SqlSessionFactory")
+@MapperScan(basePackages = DataSourceConfig1.PACKAGE, sqlSessionFactoryRef = "db01SqlSessionFactory")
 public class DataSourceConfig1 {
+    /**
+     * dao层的包路径
+     */
+    static final String PACKAGE = "com.ashen.mybatis.mapper.db01";
+    /**
+     * mapper文件的相对路径
+     */
+    private static final String MAPPER_LOCATION = "classpath*:com/ashen/mybatis/mapper/db01/*.xml";
 
-    // 将这个对象放入Spring容器中
+    private String filters;
+    private String url;
+    private String username;
+    private String password;
+    private String driverClassName;
+    private int initialSize;
+    private int minIdle;
+    private int maxActive;
+    private long maxWait;
+    private long timeBetweenEvictionRunsMillis;
+    private long minEvictableIdleTimeMillis;
+    private String validationQuery;
+    private boolean testWhileIdle;
+    private boolean testOnBorrow;
+    private boolean testOnReturn;
+    private boolean poolPreparedStatements;
+    private int maxPoolPreparedStatementPerConnectionSize;
+
+
+    // 主数据源使用@Primary注解进行标识
+    @Primary
     @Bean(name = "db01DataSource")
-    // 表示这个数据源是默认数据源
-    @Primary
-    // 读取application.properties中的配置参数映射成为一个对象
-    // prefix表示参数的前缀
-    @ConfigurationProperties(prefix = "spring.datasource.db01")
-    public DataSource getDateSource1() {
-        return DataSourceBuilder.create().build();
+    public DataSource primaryDataSource() throws SQLException {
+        DruidDataSource druid = new DruidDataSource();
+        // 监控统计拦截的filters
+        druid.setFilters(filters);
+
+        // 配置基本属性
+        druid.setDriverClassName(driverClassName);
+        druid.setUsername(username);
+        druid.setPassword(password);
+        druid.setUrl(url);
+
+        //初始化时建立物理连接的个数
+        druid.setInitialSize(initialSize);
+        //最大连接池数量
+        druid.setMaxActive(maxActive);
+        //最小连接池数量
+        druid.setMinIdle(minIdle);
+        //获取连接时最大等待时间，单位毫秒。
+        druid.setMaxWait(maxWait);
+        //间隔多久进行一次检测，检测需要关闭的空闲连接
+        druid.setTimeBetweenEvictionRunsMillis(timeBetweenEvictionRunsMillis);
+        //一个连接在池中最小生存的时间
+        druid.setMinEvictableIdleTimeMillis(minEvictableIdleTimeMillis);
+        //用来检测连接是否有效的sql
+        druid.setValidationQuery(validationQuery);
+        //建议配置为true，不影响性能，并且保证安全性。
+        druid.setTestWhileIdle(testWhileIdle);
+        //申请连接时执行validationQuery检测连接是否有效
+        druid.setTestOnBorrow(testOnBorrow);
+        druid.setTestOnReturn(testOnReturn);
+        //是否缓存preparedStatement，也就是PSCache，oracle设为true，mysql设为false。分库分表较多推荐设置为false
+        druid.setPoolPreparedStatements(poolPreparedStatements);
+        // 打开PSCache时，指定每个连接上PSCache的大小
+        druid.setMaxPoolPreparedStatementPerConnectionSize(maxPoolPreparedStatementPerConnectionSize);
+
+        return druid;
     }
 
+    // 创建该数据源的事务管理
+    /*
+    @Primary
+    @Bean(name = "db01TransactionManager")
+    public DataSourceTransactionManager primaryTransactionManager() throws SQLException {
+        return new DataSourceTransactionManager(primaryDataSource());
+    }
+    */
+
+
+    /**
+     * 创建Mybatis的连接会话工厂实例
+     *
+     * @param db01DataSource
+     * @return
+     * @throws Exception
+     */
+    @Primary
     @Bean(name = "db01SqlSessionFactory")
-    // 表示这个数据源是默认数据源
-    @Primary
-    // @Qualifier表示查找Spring容器中名字为db01DataSource的对象
-    public SqlSessionFactory db01SqlSessionFactory(@Qualifier("db01DataSource") DataSource datasource)
-            throws Exception {
-        SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
-        bean.setDataSource(datasource);
-        bean.setMapperLocations(
-                // 设置mybatis的xml所在位置
-                new PathMatchingResourcePatternResolver().getResources("classpath*:com/ashen/mybatis/mapper/db01/*.xml"));
-        return bean.getObject();
+    public SqlSessionFactory db01SqlSessionFactory(@Qualifier("db01DataSource") DataSource db01DataSource) throws Exception {
+        final SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        // 设置数据源bean
+        sessionFactory.setDataSource(db01DataSource);
+        // 设置mapper文件路径
+        sessionFactory.setMapperLocations(new PathMatchingResourcePatternResolver()
+                .getResources(DataSourceConfig1.MAPPER_LOCATION));
+
+        return sessionFactory.getObject();
     }
 
-    @Bean("db01SqlSessionTemplate")
-    // 表示这个数据源是默认数据源
-    @Primary
-    public SqlSessionTemplate db01SqlSessionTemplate(
-            @Qualifier("db01SqlSessionFactory") SqlSessionFactory sessionFactory) {
-        return new SqlSessionTemplate(sessionFactory);
-    }
 }
