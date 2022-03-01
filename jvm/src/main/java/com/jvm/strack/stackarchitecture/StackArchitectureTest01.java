7e7eae33af30cbfc53661eca536c98d8f56429f5
2504816574 <2504816574@qq.com>
2022-03-02 00:33:22 +0800

    操作数栈

---
+++ jvm/src/main/java/com/jvm/strack/stackarchitecture/StackArchitectureTest01.java
@@ -0,0 +1,12 @@
+package com.jvm.strack.stackarchitecture;
+
+/**
+ * @author sdong
+ */
+
+public class StackArchitectureTest01 {
+
+    public static void main(String[] args) {
+        int i = 2 + 3;
+    }
+}
---
+++ jvm/src/main/java/com/jvm/strack/stackarchitecture/StackArchitectureTest02.java
@@ -0,0 +1,14 @@
+package com.jvm.strack.stackarchitecture;
+
+/**
+ * @author sdong
+ */
+
+public class StackArchitectureTest02 {
+
+    public static void main(String[] args) {
+        int i = 2;
+        int j = 3;
+        int k = i + j;
+    }
+}
