d260e7e04dc5ff189147152f61863b61dbf8d9a0
2504816574 <2504816574@qq.com>
2022-03-02 23:54:32 +0800

    初始化

---
+++ jvm/src/main/java/com/jvm/classloadingprocess/ClassInitTest04.java
@@ -0,0 +1,35 @@
+package com.jvm.classloadingprocess;
+
+/**
+ * @author sdong
+ * @description TODO
+ * @date 2022/3/2 23:18
+ */
+public class ClassInitTest04 {
+
+    public static void main(String[] args) {
+        Runnable runnable = () -> {
+            System.out.println(Thread.currentThread().getName() + "开始");
+            DeadTest deadTest = new DeadTest();
+            System.out.println(Thread.currentThread().getName() + "结束");
+        };
+        Thread thread1 = new Thread(runnable, "线程1");
+        Thread thread2 = new Thread(runnable, "线程2");
+        thread1.start();
+        thread2.start();
+    }
+
+}
+
+class DeadTest {
+
+    static {
+        if (true) {
+            System.out.println(Thread.currentThread().getName() + "初始化当前类");
+            //让线程一直处于初始化状态
+            while (true) {
+
+            }
+        }
+    }
+}
